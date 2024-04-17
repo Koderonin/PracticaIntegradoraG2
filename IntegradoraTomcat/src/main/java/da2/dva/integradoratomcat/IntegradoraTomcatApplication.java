7fc20129f1fbea8be245f37aa319d34321082c23
AliciaSJF <aliciasj.2003@gmail.com>
2024-04-17 11:09:16 +0200

    Creado proyecto tomcat

---
+++ IntegradoraTomcat/src/main/java/da2/dva/integradoratomcat/IntegradoraTomcatApplication.java
@@ -0,0 +1,13 @@
+package da2.dva.integradoratomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegradoraTomcatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegradoraTomcatApplication.class, args);
+    }
+
+}
