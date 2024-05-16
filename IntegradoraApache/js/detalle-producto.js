7cbbd1f942acce9653c6daad45257289dbab8e42
drecioa <diegorecio22@outlook.es>
2024-05-16 20:20:47 +0200

    feat(Detalle Producto)
    Implementada vista de detalle del producto desde el listado de productos

--- IntegradoraApache/js/detalle-producto.js
+++ IntegradoraApache/js/detalle-producto.js
@@ -1,60 +1,53 @@
-const containerProductos = document.querySelector('.container-items');
+const USER_EMAIL = $('.user-email');
+const USER_IMG = $('.user-img').children();
+const DIV_PERSONAL_INFO = $('.personal-info2')[0];
+const DIV_DIRECCION_INFO = $('.direccion-info2')[0];
 
-if(containerProductos != null){
-	listarProductos();
-}
+async function datosCliente() {
+	//$.ajaxSetup({xhrFields: { withCredentials: true } });
+	let codigo = location.search.split('=')[1];
+	let url = 'http://tomcat.da2.dva:8080/api/producto/detalle/' + codigo;
 
-function crearItemProducto(producto) {
-	let div = document.createElement('div'); div.classList.add('item');
-	let figure = document.createElement('figure');
-	let img = document.createElement('img'); img.alt = 'Imagen' + producto.modelo; img.src = devolverImagen(producto); //TODO: Obtener imagen
-	let div2 = document.createElement('div'); div2.classList.add('info-product');
-	let h2 = document.createElement('h2'); h2.textContent = producto.modelo;
-	let p = document.createElement('p'); p.classList.add('price'); p.textContent = producto.precio + ' €';
-	let button = document.createElement('button'); button.classList.add('btn-add-cart'); button.textContent = 'Añadir al carrito';
-	div2.append(h2, p, button); figure.append(img); div.append(figure, div2);
+	$.getJSON(url, function(response) {
+		//response = response[0];
 
-	div2.style = 'cursor: pointer;';
-	div2.addEventListener('click', () => {
-		location.href="producto-info.html?codigo="+producto.codigo;
-	})
-    return div;
-}
+		USER_IMG.attr('src', response.imagenes[0]);
+		USER_EMAIL.text(response.marca + ' ' + response.modelo);
 
+		DIV_PERSONAL_INFO.appendChild(document.createElement('p')).innerText = response.marca;
+		DIV_PERSONAL_INFO.appendChild(document.createElement('p')).innerText = response.modelo;
+		DIV_PERSONAL_INFO.appendChild(document.createElement('p')).innerText = response.descripcion;
+		DIV_PERSONAL_INFO.appendChild(document.createElement('p')).innerText = response.precio + '€';
+		DIV_PERSONAL_INFO.appendChild(document.createElement('p')).innerText = response.cantidadAlmacen;
+		DIV_PERSONAL_INFO.appendChild(document.createElement('p')).innerText = (response.enOferta) ? 'Sí' : 'No';
+		DIV_PERSONAL_INFO.appendChild(document.createElement('p')).innerText = (response.esNovedad) ? 'Sí' : 'No';
+		DIV_PERSONAL_INFO.appendChild(document.createElement('p')).innerText = response.comentarios;
 
-
-function listarProductos(){
-	containerProductos.innerHTML = '<div id="search-mobile" class="input-container"><input type="text" id="input" name="searchBox" placeholder="Search for Clothing and Accessories" class="input search-input"></div>';
-	$.getJSON("http://tomcat.da2.dva:8080/api/producto/listado", function(result) {
-		$.each(result, function(i, field){
-        	containerProductos.appendChild(crearItemProducto(field));
-        });
+		$.each(response.imagenes, function(key, value) {
+			if (key !== 0) {
+				let div = document.createElement('div'); div.classList.add('user-img');
+				let img = document.createElement('img'); img.src = value;
+				div.appendChild(img);
+				DIV_PERSONAL_INFO.appendChild(div);
+			}
+		})
 	});
 }
 
-function devolverImagen(producto){
-
-	if(producto.imagenes == null){
-		return "https://placehold.co/600x400"
-	} else {
-		return producto.imagenes[0];
-	}
-}
+datosCliente();
 
 
+/*
 // Selecciona el botón del carrito y el contenedor de productos del carrito del DOM
 const btnCart = document.querySelector('.container-cart-icon');
 const containerCartProducts = document.querySelector('.container-cart-products');
-const dropdownMenu = document.getElementById("dropdownMenu");
 
 // Agrega un evento de clic al botón del carrito para alternar la visibilidad del contenedor de productos del carrito
 btnCart.addEventListener('click', () => {
 	containerCartProducts.classList.toggle('hidden-cart');
-	dropdownMenu.classList.remove("show");
-	
 });
 
-/* ========================= */
+/* ========================= 
 // Declaración de variables para interactuar con elementos del DOM
 
 // Elementos relacionados con la información del producto y el carrito
@@ -67,7 +60,6 @@ const cartTotal = document.querySelector('.cart-total');
 const productsList = document.querySelector('.container-items');
 const countProducts = document.querySelector('#contador-productos');
 const valorTotal = document.querySelector('.total-pagar');
-
 
 // Array que almacena los productos en el carrito
 let allProducts = [];
@@ -187,36 +179,5 @@ const showHTML = () => {
 	// Actualiza el total a pagar y el contador de productos
 	valorTotal.innerText = `$${total}`;
 	countProducts.innerText = totalOfProducts;
-};
+};*/
 
-/*--------------------------------- DESPLEGABLE AREA CLIENTE ---------------------------------*/
-function toggleDropdown() {
-    dropdownMenu.classList.toggle("show");
-	containerCartProducts.classList.add('hidden-cart');
-	
-}
-
-// Cerrar el menú desplegable si el usuario hace clic fuera de él
-window.onclick = function(event) {
-    if (!event.target.matches('.userIcon')) {
-        let dropdowns = document.getElementsByClassName("dropdown-content");
-        for (let i = 0; i < dropdowns.length; i++) {
-            let openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
-
-
-//--------------------------------- MENU HAMBURGUESA ---------------------------------
-function openNav(x) {
-	document.getElementById("mySidenav").classList.toggle("show");
-	x.classList.toggle("change");
-  }
-//--------------------------------- OFERTAS ---------------------------------
-
-
-            
-       
