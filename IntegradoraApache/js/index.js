34c29742005391ba201d63f661c8ff9ac16f65a4
drecioa <diegorecio22@outlook.es>
2024-05-16 13:51:38 +0200

    fix(Merge): Arreglados errores provocados por el merge

--- IntegradoraApache/js/index.js
+++ IntegradoraApache/js/index.js
@@ -4,31 +4,25 @@ if(containerProductos != null){
 	listarProductos();
 }
 
-
-
 function crearItemProducto(producto) {
-    let itemHTML = `
-        <div class="item">
-            <figure>
-                <img src="https://via.placeholder.com/300" class="product-image" alt="producto">
-            </figure>
-            <div class="info-product">
-                <h2>${producto.descripcion}</h2>
-                <p class="price">$${producto.precio}</p>
-                <button class="btn-add-cart">Añadir al carrito</button>
-            </div>
-        </div>
-    `;
-    return itemHTML;
+	let div = document.createElement('div'); div.classList.add('item');
+	let figure = document.createElement('figure');
+	let img = document.createElement('img'); img.alt = 'Imagen' + producto.modelo; img.src = devolverImagen(producto); //TODO: Obtener imagen
+	let div2 = document.createElement('div'); div2.classList.add('info-product');
+	let h2 = document.createElement('h2'); h2.textContent = producto.modelo;
+	let p = document.createElement('p'); p.classList.add('price'); p.textContent = producto.precio + ' €';
+	let button = document.createElement('button'); button.classList.add('btn-add-cart'); button.textContent = 'Añadir al carrito';
+	div2.append(h2, p, button); figure.append(img); div.append(figure, div2);
+    return div;
 }
 
 
 
-async function listarProductos(){
+function listarProductos(){
+	containerProductos.innerHTML = '<div id="search-mobile" class="input-container"><input type="text" id="input" name="searchBox" placeholder="Search for Clothing and Accessories" class="input search-input"></div>';
 	$.getJSON("http://tomcat.da2.dva:8080/api/producto/listado", function(result) {
 		$.each(result, function(i, field){
-            let itemHTML = crearItemProducto(field);
-        	containerProductos.innerHTML += itemHTML;
+        	containerProductos.appendChild(crearItemProducto(field));
         });
 	});
 }
@@ -43,7 +37,6 @@ function devolverImagen(producto){
 }
 
 
-
 // Selecciona el botón del carrito y el contenedor de productos del carrito del DOM
 const btnCart = document.querySelector('.container-cart-icon');
 const containerCartProducts = document.querySelector('.container-cart-products');
@@ -53,7 +46,7 @@ const dropdownMenu = document.getElementById("dropdownMenu");
 btnCart.addEventListener('click', () => {
 	containerCartProducts.classList.toggle('hidden-cart');
 	dropdownMenu.classList.remove("show");
-
+	
 });
 
 /* ========================= */
@@ -69,6 +62,7 @@ const cartTotal = document.querySelector('.cart-total');
 const productsList = document.querySelector('.container-items');
 const countProducts = document.querySelector('#contador-productos');
 const valorTotal = document.querySelector('.total-pagar');
+
 
 // Array que almacena los productos en el carrito
 let allProducts = [];
@@ -194,7 +188,7 @@ const showHTML = () => {
 function toggleDropdown() {
     dropdownMenu.classList.toggle("show");
 	containerCartProducts.classList.add('hidden-cart');
-
+	
 }
 
 // Cerrar el menú desplegable si el usuario hace clic fuera de él
@@ -219,4 +213,5 @@ function openNav(x) {
 //--------------------------------- OFERTAS ---------------------------------
 
 
-
+            
+       
