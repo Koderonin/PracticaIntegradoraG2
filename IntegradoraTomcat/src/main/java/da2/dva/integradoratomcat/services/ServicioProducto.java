acbfd39df474aee7dc537a715eb62f1c423919f1
Koderonin <134518981+Koderonin@users.noreply.github.com>
2024-05-16 17:40:03 +0200

    Arreglado tema imágenes a falta de remates
    Ahora el formulario de producto está un pelín más estructurado

--- IntegradoraTomcat/src/main/java/da2/dva/integradoratomcat/services/ServicioProducto.java
+++ IntegradoraTomcat/src/main/java/da2/dva/integradoratomcat/services/ServicioProducto.java
@@ -117,5 +117,8 @@ public class ServicioProducto {
         return mongoTemplate.findOne(query, Document.class, "producto");
     }
 
+    public void deleteAll() {
+        mongoTemplate.dropCollection("producto");
+    }
 
 }
